db3568d354c009625ac84ba72572c0a903bf35a6
stephanie <stephieplusthree@gmail.com>
2020-11-16 16:51:00 -0800

    navbar css added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import * as AiIcons from "react-icons/ai";
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import './Navbar.css';
+import { IconContext } from 'react-icons';
 
 function Navbar() {
     const [sidebar, setSidebar] = useState(false)
@@ -12,6 +13,7 @@ function Navbar() {
 
     return (
         <>
+        <IconContext.Provider value={{ color: '#fff' }}>
             <div className="navbar">
                 <Link to="#" className="menu-bars">
                     <FaIcons.FaBars onClick={showSidebar} />
@@ -36,6 +38,7 @@ function Navbar() {
                     })}
                 </ul>
             </nav>
+        </IconContext.Provider>
         </>
     )
 }
